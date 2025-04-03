177885f5c2252db2f7974c8b1ca47e3280b7a6f5
flurion014 <flamina02@gmail.com>
2025-04-03 09:10:11 +0200

    Update 5-compuesto.js

--- jsTests/5-compuesto.js
+++ jsTests/5-compuesto.js
@@ -8,12 +8,16 @@ let objeto = {
   funcion: function(x){return objeto.numero + x;}
 };
 
+//uso de bucle for e if...else if...else a la vez
+//aprovechamos para incluir tambien typeof, comparativas, objetos, funciones...
+//la evolucion de objeto.numero es 0,1,2,1,0; en caso de error vale 5 (o 0, pero palabra no pasa el check)
+//la evolucion de objeto.palabra es pa,pal,pala,palab,palabr,palabra; en caso de error sobraran y/o faltaran letras
 let borde = 10;
 for(let i=0;i<borde;i+=2){
   if(i<2){
     objeto.palabra += "l";
     if(typeof objeto.palabra === "string"){
-      objeto.numero = objeto.funcion(1);
+      objeto.numero = 1;
     } else {
       objeto.numero = 5;
     }
